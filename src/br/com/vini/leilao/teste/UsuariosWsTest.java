897a093a697bfbfea544339278e6ab62a3ff2516
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-04-23 09:13:15 -0300

    testando JSONs

--- src/br/com/vini/leilao/teste/UsuariosWsTest.java
+++ src/br/com/vini/leilao/teste/UsuariosWsTest.java
@@ -5,13 +5,24 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import com.jayway.restassured.path.json.JsonPath;
 import com.jayway.restassured.path.xml.XmlPath;
 
 import br.com.vini.leilao.modelo.Usuario;
 
 public class UsuariosWsTest {
+	
+	private Usuario mauricio;
+	private Usuario guilherme;
+
+	@Before
+	public void setUp() {
+		mauricio = new Usuario(1l,"Mauricio Aniche", "mauricio.aniche@caelum.com.br");
+		guilherme = new Usuario(2l,"Guilherme Silveira", "guilherme.silveira@caelum.com.br");
+	}
 	
 	@Test
 	public void deveRetornarListaDeUsuario() {
@@ -22,11 +33,26 @@ public class UsuariosWsTest {
 		
 		List<Usuario> usuarios = xmlPath.getList("list.usuario",Usuario.class);
 		
-		Usuario esperado1 = new Usuario(1l,"Mauricio Aniche", "mauricio.aniche@caelum.com.br");
-		Usuario esperado2 = new Usuario(2l,"Guilherme Silveira", "guilherme.silveira@caelum.com.br");
+		assertEquals(mauricio, usuarios.get(0));
+		assertEquals(guilherme, usuarios.get(1));
+	}
+	
+	
+	@Test
+	public void deveRetornarUsuarioPeloId() {
+		JsonPath jsonPath =
+				given()
+					.header("Accept", "application/json")
+					.parameter("usuario.id", 1)
+					.get("/usuarios/show")
+					.andReturn().jsonPath();
 		
-		assertEquals(esperado1, usuarios.get(0));
-		assertEquals(esperado2, usuarios.get(1));
+		
+		Usuario usuario = jsonPath.getObject("usuario", Usuario.class);
+		
+		//System.out.println(jsonPath.getString("usuario.nome"));
+		
+		assertEquals(usuario, mauricio);
 	}
 
 }
